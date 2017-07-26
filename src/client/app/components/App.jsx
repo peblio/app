d3abd08d0fd4cb8c2a71df013ae88a3d1e7157ea
mathuramg <mmg9291@gmail.com>
2017-07-26 16:58:32 -0400

    move states to app.jsx

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -4,11 +4,24 @@ import { render } from 'react-dom';
 import EditorContainer from './EditorContainer.jsx';
 import TextArea from './TextArea.jsx';
 import TextToolbar from './TextToolbar.jsx';
+import {Editor, EditorState, RichUtils, Modifier} from 'draft-js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      editorState: EditorState.createEmpty(),
+      styleMap: {
+        'STRIKETHROUGH': {
+          textDecoration: 'line-through',
+        },
+        'FONT': {
+          fontSize: '54pt',
+        },
+        'FONTFACE': {
+          fontFamily: 'monospace',
+        }
+      },
       code: "",
       isPlaying: false,
       editorMode: {
@@ -21,6 +34,15 @@ class App extends React.Component {
     this.playCode = this.playCode.bind(this);
     this.updateCode = this.updateCode.bind(this);
     this.setEditorMode = this.setEditorMode.bind(this);
+
+    this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this._onBoldClick = this._onBoldClick.bind(this);
+    this._onItalicClick = this._onItalicClick.bind(this);
+    this._onUnderlineClick = this._onUnderlineClick.bind(this);
+    this._onCodeClick = this._onCodeClick.bind(this);
+    this._onFontChange = this._onFontChange.bind(this);
+    this._onFontfaceChange = this._onFontfaceChange.bind(this);
   }
   setEditorMode(event) {
     console.log(event.target.value);
@@ -50,7 +72,47 @@ class App extends React.Component {
       })
     }
   }
-
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+  _onUnderlineClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+  _onCodeClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
+  }
+  _onFontChange(event) {
+    const newFontSize = event.target.value + 'pt';
+    console.log(newFontSize);
+    const tempStyleMap = this.state.styleMap;
+    tempStyleMap.FONT.fontSize = newFontSize;
+    this.setState({styleMap: tempStyleMap});
+    const selection = this.state.editorState.getSelection();
+    const content = this.state.editorState.getCurrentContent();
+    const newContent = Modifier.applyInlineStyle(content, selection, 'FONT');
+    this.onChange(EditorState.push(this.state.editorState, newContent, 'change-inline-style'));
+  }
+  _onFontfaceChange(event) {
+    const newFontface = event.target.value;
+    const tempStyleMap = this.state.styleMap;
+    tempStyleMap.FONTFACE.fontFamily = newFontface;
+    this.setState({styleMap: tempStyleMap});
+    const selection = this.state.editorState.getSelection();
+    const content = this.state.editorState.getCurrentContent();
+    const newContent = Modifier.applyInlineStyle(content, selection, 'FONTFACE');
+    this.onChange(EditorState.push(this.state.editorState, newContent, 'change-inline-style'));
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
   updateCode(value) {
     this.setState({
       code: value
@@ -82,7 +144,18 @@ class App extends React.Component {
             editorMode = {this.state.editorMode}
             setEditorMode= {this.setEditorMode}
           />
-          <TextArea/>
+          <TextArea
+            editorState = {this.state.editorState}
+            styleMap = {this.state.styleMap}
+            onChange = {this.onChange}
+            handleKeyCommand = {this.handleKeyCommand}
+            _onBoldClick = {this._onBoldClick}
+            _onItalicClick = {this._onItalicClick}
+            _onUnderlineClick = {this._onUnderlineClick}
+            _onCodeClick = {this._onCodeClick}
+            _onFontChange = {this._onFontChange}
+            _onFontfaceChange = {this._onFontfaceChange}
+          />
         </section>
       </div>
     );
--- src/client/app/components/TextArea.jsx
+++ src/client/app/components/TextArea.jsx
@@ -11,104 +11,40 @@ const styleMap = {
 class TextArea extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      editorState: EditorState.createEmpty(),
-      styleMap: {
-        'STRIKETHROUGH': {
-          textDecoration: 'line-through',
-        },
-        'FONT': {
-          fontSize: '54pt',
-        },
-        'FONTFACE': {
-          fontFamily: 'monospace',
-        }
-      }
-    };
-    this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-    this._onBoldClick = this._onBoldClick.bind(this);
-    this._onItalicClick = this._onItalicClick.bind(this);
-    this._onUnderlineClick = this._onUnderlineClick.bind(this);
-    this._onCodeClick = this._onCodeClick.bind(this);
-    this._onFontChange = this._onFontChange.bind(this);
-    this._onFontfaceChange = this._onFontfaceChange.bind(this);
+    console.log('test');
   }
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }
-  _onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
-  }
-  _onUnderlineClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-  }
-  _onCodeClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'STRIKETHROUGH'));
-  }
-  _onFontChange(event) {
-    const newFontSize = event.target.value + 'pt';
-    console.log(newFontSize);
-    const tempStyleMap = this.state.styleMap;
-    tempStyleMap.FONT.fontSize = newFontSize;
-    this.setState(styleMap: tempStyleMap);
-    const selection = this.state.editorState.getSelection();
-    const content = this.state.editorState.getCurrentContent();
-    const newContent = Modifier.applyInlineStyle(content, selection, 'FONT');
-    this.onChange(EditorState.push(this.state.editorState, newContent, 'change-inline-style'));
-  }
-  _onFontfaceChange(event) {
-    const newFontface = event.target.value;
-    const tempStyleMap = this.state.styleMap;
-    tempStyleMap.FONTFACE.fontFamily = newFontface;
-    this.setState(styleMap: tempStyleMap);
-    const selection = this.state.editorState.getSelection();
-    const content = this.state.editorState.getCurrentContent();
-    const newContent = Modifier.applyInlineStyle(content, selection, 'FONTFACE');
-    this.onChange(EditorState.push(this.state.editorState, newContent, 'change-inline-style'));
-  }
-  handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
-    }
-    return 'not-handled';
-  }
-  componentDidUpdate() {
 
-    console.log(this.state.editorState);
-  }
+
   render() {
     return (
       <div>
         <div>
           <button
-            onClick={this._onBoldClick}
+            onClick={this.props._onBoldClick}
           >
             B
           </button>
           <button
-            onClick={this._onUnderlineClick}
+            onClick={this.props._onUnderlineClick}
           >
             U
           </button>
           <button
-            onClick={this._onItalicClick}
+            onClick={this.props._onItalicClick}
           >
             I
           </button>
           <button
-            onClick={this._onCodeClick}
+            onClick={this.props._onCodeClick}
           >
             -
           </button>
-          <select name="fontface" onChange={this._onFontfaceChange}>
+          <select name="fontface" onChange={this.props._onFontfaceChange}>
             <option value="Arial, Helvetica, sans-serif">Arial</option>
             <option value="Impact, Charcoal, sans-serif">Impact</option>
             <option value="Times New Roman, Times, serif">Times New Roman</option>
           </select>
-          <select name="font-size" onChange={this._onFontChange}>
+          <select name="font-size" onChange={this.props._onFontChange}>
             <option value="20">20</option>
             <option value="24">24</option>
             <option value="32">32</option>
@@ -117,11 +53,11 @@ class TextArea extends React.Component {
         </div>
 
         <Editor
-          customStyleMap={this.state.styleMap}
-          editorState={this.state.editorState}
-          onChange={this.onChange}
+          customStyleMap={this.props.styleMap}
+          editorState={this.props.editorState}
+          onChange={this.props.onChange}
           placeholder="Enter some text..."
-          handleKeyCommand={this.handleKeyCommand}
+          handleKeyCommand={this.props.handleKeyCommand}
         />
       </div>
     );
