168dc09614865f5dcd1deff762971c9d02bef390
mathuramg <mmg9291@gmail.com>
2017-08-29 09:59:18 -0400

    setup for heroku

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -5,15 +5,15 @@ const express = require('express'); // include the express library
 const app = express();
 
 // start the server:
-app.listen(8000);
+app.listen(process.env.PORT ||8080);
 app.use('/', express.static('src/client/')); // set a static file directory
 
-mongoose.connect('mongodb://localhost:27017/p5js-web-editor');
-mongoose.connection.on('error', () => {
-  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
-mongoose.connection.on('open', () => {
-  console.log('MongoDB Connection success.');
-  // process.exit(1);
-});
+// mongoose.connect('mongodb://localhost:27017/p5js-web-editor');
+// mongoose.connection.on('error', () => {
+//   console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+//   process.exit(1);
+// });
+// mongoose.connection.on('open', () => {
+//   console.log('MongoDB Connection success.');
+//   // process.exit(1);
+// });
