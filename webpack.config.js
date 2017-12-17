01da4df868730bba057aea7dfc14051dedce657c
mathuramg <mmg9291@gmail.com>
2017-12-17 11:09:32 -0500

    lint on webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+const APP_DIR = path.resolve(__dirname, 'src/client/app');
 
-var config = {
-  entry: APP_DIR + '/index.jsx',
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -24,15 +24,16 @@ var config = {
       },
       {
         test: /\.(scss|css)$/,
-        loaders: ['style-loader', 'css-loader','sass-loader']
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.svg$/,
         loader: 'svg-react-loader'
       }
     ]
-  }, node: {
-    fs: "empty"
+  },
+  node: {
+    fs: 'empty'
   }
 };
 
