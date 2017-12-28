8c0a5f29af9e8e04b348e793116e2f9a7d430d6e
Esther Hersh <estherhersh@Esthers-MacBook-Pro.local>
2017-12-28 14:02:49 -0500

    just preliminary design outline

--- src/client/app/components/MainToolbar.jsx
+++ src/client/app/components/MainToolbar.jsx
@@ -54,14 +54,10 @@ class MainToolbar extends React.Component {
     return (
       <div>
         <div className="mainToolbar">
-          <input
-            className="mainToolbar__title"
-            placeholder="Title"
-            type="text" value={this.props.pageTitle} onChange={this.props.setPageTitle}
-          ></input>
+
           <div className="mainToolbar__div-left">
             <div className="fileModal__container">
-              <button className="mainToolbar__save" onClick={this.props.toggleFileDropdown}>
+              <button className="upperToolbar__dropdown" onClick={this.props.toggleFileDropdown}>
               File
               </button>
             {(() => { // eslint-disable-line
@@ -77,16 +73,22 @@ class MainToolbar extends React.Component {
             })()}
             </div>
           </div>
+          <input
+            className="mainToolbar__title"
+            placeholder="Title"
+            type="text" value={this.props.pageTitle} onChange={this.props.setPageTitle}
+          ></input>
           <div className="mainToolbar__div-right">
         {(()=> { // eslint-disable-line
           if (this.props.name) {
             return (
               <div>
-                <p className="mainToolbar__welcome">
-                    Welcome {this.props.name}!
-                </p>
+
 
                 <a className="mainToolbar__save" href="/logout">Logout</a>
+                  <p className="mainToolbar__welcome">
+                      Welcome {this.props.name}!
+                  </p>
               </div>
 
             );
