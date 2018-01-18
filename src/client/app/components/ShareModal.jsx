03a7a2db71c085383ad67e890fccc73288c330fb
mathuramg <mmg9291@gmail.com>
2018-01-17 20:29:10 -0500

    add share modal

--- src/client/app/components/ShareModal.jsx
+++ src/client/app/components/ShareModal.jsx
@@ -1,11 +1,27 @@
 import React, { PropTypes } from 'react';
 
 class Share extends React.Component {
+  constructor(props) {
+    super(props);
+    this.copyShareLink = this.copyShareLink.bind(this);
+  }
+
+  copyShareLink() {
+    this.input.select();
+    try {
+      const successful = document.execCommand('copy');
+      const msg = successful ? 'successful' : 'unsuccessful';
+      console.log(`Copying text command was ${msg}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return (
       <div className="share__container">
-        <input className="share__input"></input>
-        <button className="share__button">Copy Link</button>
+        <input className="share__input" ref={(element) => { this.input = element; }} value={window.location.href} readOnly></input>
+        <button className="share__button" onClick={this.copyShareLink}>Copy Link</button>
       </div>
     );
   }
