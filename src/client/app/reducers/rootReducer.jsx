fe52b5a84e7893ef0a0a360cd5a33c6f9230d831
mathuramg <mmg9291@gmail.com>
2018-01-24 19:18:07 -0500

    remove p5files.jsx

--- src/client/app/reducers/rootReducer.jsx
+++ src/client/app/reducers/rootReducer.jsx
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 
 import editors from './editors.jsx';
 import mainToolbar from './mainToolbar.jsx';
-import p5files from './p5files.jsx';
 import page from './page.jsx';
 import user from './user.jsx';
 
 const rootReducer = combineReducers({
   editors,
   mainToolbar,
-  p5files,
   page,
   user
 });
