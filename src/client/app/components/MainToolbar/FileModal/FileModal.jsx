52653fd298b342a19b618078aeeb7b5cecd1cb5f
mathuramg <mmg9291@gmail.com>
2018-06-05 13:14:27 -0400

    add back style sheet

--- src/client/app/components/MainToolbar/FileModal/FileModal.jsx
+++ src/client/app/components/MainToolbar/FileModal/FileModal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// require('./fileModal.scss');
+require('./fileModal.scss');
 
 class FileModal extends React.Component {
   render() {
