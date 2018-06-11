42ea067a5366fafdc483df20649834db62cc2675
Oren Shoham <oren.avraham.shoham@gmail.com>
2018-06-10 23:46:08 -0400

    configure webpack to load .env file, use source maps, and serve images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
-const sassLintPlugin = require('sasslint-webpack-plugin');
+const SassLintPlugin = require('sasslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 const APP_DIR = path.resolve(__dirname, 'src/client/app');
@@ -40,29 +41,34 @@ const config = {
                 path.resolve(APP_DIR, 'styles/sass'),
               ],
               sourceMap: true
-
             }
           }
         ]
-
       },
       {
         test: /\.svg$/,
         loader: 'svg-react-loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ['url-loader']
       }
     ]
   },
   node: {
     fs: 'empty'
   },
+  devtool: 'source-map',
   plugins: [
-    new sassLintPlugin({
+    new SassLintPlugin({
       configFile: '.sass-lint.yml',
       glob: 'src/**/*.s?(a|c)ss',
     }),
     new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
-    })
+      compress: { warnings: false },
+      sourceMap: true
+    }),
+    new Dotenv()
   ]
 };
 
