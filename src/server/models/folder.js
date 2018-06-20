8a4719e2e944e050b9e1558b4c9f9695d36b085c
Oren Shoham <oren.avraham.shoham@gmail.com>
2018-06-20 14:30:34 -0400

    use virtuals to populate one-to-many relationships for Folder model

--- src/server/models/folder.js
+++ src/server/models/folder.js
@@ -4,11 +4,26 @@ const autopopulate = require('mongoose-autopopulate');
 const Schema = mongoose.Schema;
 
 const folderSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   parent: { type: Schema.Types.ObjectId, ref: 'Folder' },
-  children: [{ type: Schema.Types.ObjectId, ref: 'Folder', autopopulate: { maxDepth: 10 } }],
-  files: [{ type: Schema.Types.ObjectId, ref: 'Page', autopopulate: true }],
   title: { type: String, default: '' },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+
+folderSchema.virtual('children', {
+  ref: 'Folder',
+  localField: '_id',
+  foreignField: 'parent',
+  autopopulate: { maxDepth: 10 } // maxDepth is the limit for folder nesting
+});
+
+folderSchema.virtual('files', {
+  ref: 'Page',
+  localField: '_id',
+  foreignField: 'folder',
+  autopopulate: true
 });
 
 folderSchema.plugin(autopopulate);
