873bc823713f5929d70b23db7054bcf359fa841b
Oren Shoham <oren.avraham.shoham@gmail.com>
2018-06-29 18:39:29 -0400

    Remove commented-out code for drag-and-drop functionality

--- src/client/app/components/Modal/PagesList/PageRow.jsx
+++ src/client/app/components/Modal/PagesList/PageRow.jsx
@@ -2,36 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import { DragSource } from 'react-dnd';
 
-import ItemTypes from './itemTypes.js';
 import formatDate from '../../../utils/format-date.js';
 import { deletePage } from '../../../action/page.js';
 import DeleteIcon from '../../../images/trash.svg';
-
-
-// const pageSource = {
-//   beginDrag(props) {
-//     return { id: props.id };
-//   },
-
-//   endDrag(props, monitor, component) {
-//     if (!monitor.didDrop()) {
-//       return;
-//     }
-
-//     const item = monitor.getItem();
-//     const dropResult = monitor.getDropResult();
-//     // CardActions.moveCardToList(item.id, dropResult.listId);
-//   }
-// };
-
-// function collect(_connect, monitor) {
-//   return {
-//     connectDragSource: _connect.dragSource(),
-//     isDragging: monitor.isDragging()
-//   };
-// }
 
 class PageRow extends Component {
   render() {
@@ -56,8 +30,6 @@ class PageRow extends Component {
 PageRow.propTypes = {
   deletePage: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired,
-  isDragging: PropTypes.bool.isRequired,
-  connectDragSource: PropTypes.func.isRequired,
   page: PropTypes.shape({}).isRequired
 };
 
@@ -65,7 +37,5 @@ const mapStateToProps = (state, ownProps) => ({
   page: state.page.pages.byId[ownProps.id]
 });
 const mapDispatchToProps = dispatch => bindActionCreators({ deletePage }, dispatch);
-
-// const DraggablePageRow = DragSource(ItemTypes.Page, pageSource, collect)(PageRow);
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageRow);
--- src/client/app/components/Modal/PagesList/itemTypes.js
+++
@@ -1,4 +0,0 @@
-export default {
-  Page: 'Page',
-  Folder: 'Folder'
-};
