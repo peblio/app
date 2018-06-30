d476d648c7614cbf1d3d805bbf9acacbacd2533c
Oren Shoham <oren.avraham.shoham@gmail.com>
2018-06-30 13:58:07 -0400

    respond to PR feedback

--- src/client/app/components/Modal/PagesList/FolderRow/index.jsx
+++ src/client/app/components/Modal/PagesList/FolderRow/index.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import formatDate from '../../../utils/format-date.js';
-import { deleteFolder } from '../../../action/page.js';
-import DeleteIcon from '../../../images/trash.svg';
+import formatDate from '../../../../utils/format-date.js';
+import { deleteFolder } from '../../../../action/page.js';
+import DeleteIcon from '../../../../images/trash.svg';
 
 class FolderRow extends Component {
   render() {
-    const { folder, id } = this.props;
+    const { folder } = this.props;
     return (
-      <tr className="pages__row" key={id}>
+      <tr className="pages__row">
         <td className="pages__col" >{folder.title}</td>
         <td className="pages__col" > {formatDate(folder.createdAt)} </td>
         <td className="pages__col" > {formatDate(folder.updatedAt)} </td>
         <td className="pages__col" >
-          <button className="pages__delete" onClick={() => { this.props.deleteFolder(id); }}>
+          <button className="pages__delete" onClick={() => { this.props.deleteFolder(folder._id); }}>
             <DeleteIcon alt="delete page" />
           </button>
         </td>
@@ -27,13 +27,9 @@ class FolderRow extends Component {
 
 FolderRow.propTypes = {
   deleteFolder: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
   folder: PropTypes.shape({}).isRequired
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  folder: state.page.folders.byId[ownProps.id]
-});
 const mapDispatchToProps = dispatch => bindActionCreators({ deleteFolder }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(FolderRow);
+export default connect(null, mapDispatchToProps)(FolderRow);
--- src/client/app/components/Modal/PagesList/PageRow/index.jsx
+++ src/client/app/components/Modal/PagesList/PageRow/index.jsx
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import formatDate from '../../../utils/format-date.js';
-import { deletePage } from '../../../action/page.js';
-import DeleteIcon from '../../../images/trash.svg';
+import formatDate from '../../../../utils/format-date.js';
+import { deletePage } from '../../../../action/page.js';
+import DeleteIcon from '../../../../images/trash.svg';
 
 class PageRow extends Component {
   render() {
-    // page.id is a shortid that is NOT the same thing as page._id or the id prop
-    const { id, page } = this.props;
+    // page.id is a shortid that is NOT the same thing as page._id
+    const { page } = this.props;
     const link = `/pebl/${page.id}`;
     return (
-      <tr className="pages__row" key={id}>
+      <tr className="pages__row">
         <td className="pages__col" > <a className="pages__link" href={link}> {page.title} </a> </td>
         <td className="pages__col" > {formatDate(page.createdAt)} </td>
         <td className="pages__col" > {formatDate(page.updatedAt)} </td>
         <td className="pages__col" >
-          <button className="pages__delete" onClick={() => { this.props.deletePage(id); }}>
+          <button className="pages__delete" onClick={() => { this.props.deletePage(page._id); }}>
             <DeleteIcon alt="delete page" />
           </button>
         </td>
@@ -29,13 +29,9 @@ class PageRow extends Component {
 
 PageRow.propTypes = {
   deletePage: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
   page: PropTypes.shape({}).isRequired
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  page: state.page.pages.byId[ownProps.id]
-});
 const mapDispatchToProps = dispatch => bindActionCreators({ deletePage }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(PageRow);
+export default connect(null, mapDispatchToProps)(PageRow);
