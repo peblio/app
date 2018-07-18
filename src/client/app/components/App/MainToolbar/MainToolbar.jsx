42e586f5340b37324e60eb01f45d509983211a2b
Utsav Chadha <utsavchadha@Utsavs-MBP.home>
2018-07-18 01:53:34 -0400

    Realigned modals + code review changes

--- src/client/app/components/App/MainToolbar/MainToolbar.jsx
+++ src/client/app/components/App/MainToolbar/MainToolbar.jsx
@@ -102,7 +102,7 @@ class MainToolbar extends React.Component {
               { this.props.name ? (
                 <div>
                   <button onClick={this.props.toggleAccountDropdown} className="main-toolbar__account-button">
-                    <AccountSVG alt="account man" />
+                    <AccountSVG className="account-man" />
                   </button>
                   { this.props.isAccountDropdownOpen &&
                   <div className="main-toolbar__account">
@@ -114,7 +114,7 @@ class MainToolbar extends React.Component {
                             onClick={this.props.toggleAccountDropdown}
                             className="main-toolbar__account-button-clicked"
                           >
-                            <AccountSVG alt="account man" />
+                            <AccountSVG className="account-man__clicked" />
                           </button>
                         </p>
                       </li>
