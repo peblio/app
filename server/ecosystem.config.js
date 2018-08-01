197ff5e5bc2841a90dac49885998c7866b2d2e7c
Oren Shoham <oren.avraham.shoham@gmail.com>
2018-08-01 17:53:56 -0400

    specify different node server port for production

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,9 +3,11 @@ module.exports = {
     name      : 'server',
     script    : 'server.js',
     env: {
+      PORT: 8081,
       NODE_ENV: 'development'
     },
     env_production : {
+      PORT: 8080,
       NODE_ENV: 'production'
     }
   }]
