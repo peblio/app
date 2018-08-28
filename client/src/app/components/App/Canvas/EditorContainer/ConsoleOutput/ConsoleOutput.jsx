4bed3bcccde3d0b087f6c6c54898672c79354308
Esther Hersh <estherhersh@gmail.com>
2018-08-28 18:31:28 -0400

    editor dark and light themes and console

--- client/src/app/components/App/Canvas/EditorContainer/ConsoleOutput/ConsoleOutput.jsx
+++ client/src/app/components/App/Canvas/EditorContainer/ConsoleOutput/ConsoleOutput.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 require('./consoleOutput.scss');
 
 class ConsoleOutput extends React.Component {
   render() {
+    const toggleButton = this.props.isConsoleOpen ? '&or;' : '&and;';
     return (
-      <div className="console_outputDiv">
-        <p className="console__output-text">
-          {' '}
-          {this.props.consoleOutputText.join('\n')}
-          {' '}
-        </p>
+      <div className="console__outputDiv">
+        <nav className="console__nav">
+          <p className="console__heading"> Console </p>
+          <button
+            className="console__toggle"
+            onClick={this.props.toggleConsole}
+          >
+            {ReactHtmlParser(toggleButton)}
+          </button>
+        </nav>
+        {this.props.isConsoleOpen && (
+          <p className="console__output-text">
+            {' '}
+            {this.props.consoleOutputText.join('\n')}
+            {' '}
+          </p>
+        )}
       </div>
     );
   }
