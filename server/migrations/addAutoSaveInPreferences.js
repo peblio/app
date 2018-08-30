9fc1268d679865cd0c4ef0e59dd6b4a5b16179d1
Manpreet Singh <designviacode@gmail.com>
2018-08-30 23:10:45 +0530

    updated user model and new preferences field in migration

--- server/migrations/addAutoSaveInPreferences.js
+++ server/migrations/addAutoSaveInPreferences.js
@@ -6,17 +6,16 @@ mongoose.Promise = Promise;
 
 console.log('Updating db...');
 mongoose.connect(process.env.MONGO_DB_PEBLIO, { useMongoClient: true })
-  .then(() => User.find({}).exec())
-  .then(users => Promise.all(users.map((user) => {
-    user.preferences.editorAutoSave = true;
-
-    return user.save();
-  })))
-  .then(() => {
-    console.log('Done!');
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+  .then(() => User.update(
+    {},
+    { $set: { 'preferences.editorAutoSave': true } },
+    { multi: true },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        console.log('Done!');
+        process.exit(0);
+      }
+    }).exec());
