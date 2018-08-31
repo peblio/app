f62710737a819eb0d849dabbb07feee12e632574
Manpreet Singh <designviacode@gmail.com>
2018-08-31 05:37:10 +0530

    additional condition to check for projectID

--- client/src/app/components/App/MainToolbar/MainToolbar.jsx
+++ client/src/app/components/App/MainToolbar/MainToolbar.jsx
@@ -17,7 +17,7 @@ class MainToolbar extends React.Component {
     this.autoSaveTimeout = setInterval(() => {
       if (
         this.props.name && this.props.canEdit && this.props.unsavedChanges &&
-        this.props.editorAutoSave
+        this.props.editorAutoSave && this.props.projectID()
       ) {
         this.props.autoSaveUnsavedChanges();
         this.props.savePage();
