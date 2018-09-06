194c84868a0370479264937de781c122309ee9eb
mathuramg <mmg9291@gmail.com>
2018-09-06 18:28:13 -0400

    fix fork bug

--- client/src/app/action/page.js
+++ client/src/app/action/page.js
@@ -78,6 +78,7 @@ export function submitPage(parentId, title, editors, editorIndex, layout) {
     layout
   }).then(() => {
     history.push(`/pebl/${id}`);
+    window.location.reload(true);
   })
     .catch(error => console.error(error));
 
