4515db0143213d9b144408f8540c068e0ca5e43d
mathuramg <mmg9291@gmail.com>
2018-10-04 07:06:55 +0530

    fix typo

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,8 +41,8 @@ function sendSignUpConfirmationMail(email, users, tokens, req) {
     to: email,
     from: process.env.PEBLIO_SENDGRID_MAIL,
     subject: 'Peblio Confirmation',
-    text: `'You are receiving this because you have signed up for peblio.\n\n' +
-    'Please click on the following link, or paste this into your browser to complete the process:\n\n${
+    text: `You are receiving this because you have signed up for peblio.\n\n
+    Please click on the following link, or paste this into your browser to complete the process:\n\n${
   resetLinks}`
   };
   sendMail(mailOptions);
