a12616566ec886c92954f6b46b15e61e516cb893
Mathura MG <mmg9291@gmail.com>
2018-10-12 16:46:33 +0530

    change gif links (#492)

--- client/src/app/constants/welcomeConstants.js
+++ client/src/app/constants/welcomeConstants.js
@@ -7,6 +7,6 @@ export const WELCOME_TOUR_CONTENT_2 = 'Forking any lesson will create your own c
 export const WELCOME_TOUR_CONTENT_3 = 'All pebls can be adapted by turning edit mode on!';
 
 
-export const WELCOME_IMAGE_LINK_1 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/r12dhUh5X_Teach-intro.gif';
-export const WELCOME_IMAGE_LINK_2 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/B1G_2Ln9m_Learn-intro.gif';
-export const WELCOME_IMAGE_LINK_3 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/rJ2on8357_Adapt-intro.gif';
+export const WELCOME_IMAGE_LINK_1 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/HkWkMZA9Q_teach_1.gif';
+export const WELCOME_IMAGE_LINK_2 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/SJwJMW0cm_learn_1.gif';
+export const WELCOME_IMAGE_LINK_3 = 'https://s3.amazonaws.com/peblio-files/Peblio%20Inc/images/rJn1MZR57_adapt_1.gif';
