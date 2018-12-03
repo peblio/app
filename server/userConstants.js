969bd021223534087a3562c1782c1df917f80264
Mathura MG <mmg9291@gmail.com>
2018-12-03 16:57:17 +0530

    Sign up clean up (#535)
    #309 : Sign up clean up

--- server/userConstants.js
+++ server/userConstants.js
@@ -1,17 +1,16 @@
 /* eslint-disable */
 module.exports = Object.freeze({
 
-  SIGN_UP_DUPLICATE_EMAIL: 'The email address you have entered is already associated with another account.',
-  SIGN_UP_DUPLICATE_USER: 'Username not available! Please try another name',
+  SIGN_UP_DUPLICATE_USER: 'Username not available! Please try again',
   SIGN_UP_FAILED: 'Sign up failed',
-  SIGN_UP_CHECK_MAIL: 'Please check mail to finish the sign up',
+  SIGN_UP_CHECK_MAIL: 'Please check your email to finish signing up',
   PROCEED_TO_LOG_IN: 'Successfully signed up! Please log in',
 
   CONFIRM_TOKEN_EXPIRED: 'We were unable to find a valid token. Your token my have expired.',
   CONFIRM_NO_USER: 'We were unable to find a user for this token.',
   CONFIRM_NO_EMAIL: 'We were unable to find a user with that email.',
   CONFIRM_USER_ALREADY_VERIFIED: 'This account has already been verified. Please log in.',
-  CONFIRM_USER_VERIFIED: 'The account has been verified. Please log in.',
+  CONFIRM_USER_VERIFIED: 'Sign up complete! Please log in.',
 
   LOGIN_FAILED: 'Login failed',
   LOGIN_SUCCESS: 'Login Successful',
