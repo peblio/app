d9010f047c2f8de4fe5dc0540a2f7321b4c32f57
Komal Ahluwalia <komalahluwalia06@gmail.com>
2018-12-17 00:01:32 +0100

    Compile ES6 to ES5 syntax before starting server. This is because babel-node is recommended to not be used in production

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,5 @@
+require("babel-core/register");
+require("babel-polyfill");
 const mongoose = require('mongoose');
 // use the native Promise object for Mongoose's promises
 mongoose.Promise = Promise;
@@ -5,12 +7,9 @@ const bodyParser = require('body-parser');
 import express from 'express';
 const passport = require('passport');
 const cors = require('cors');
-
 const app = express();
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-
 const currentUserRoutes = require('./controllers/currentUserController');
 const userRoutes = require('./controllers/userController.js');
 const pageRoutes = require('./controllers/pageController.js');
