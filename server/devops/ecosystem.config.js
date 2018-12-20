c545b9bd019b59bea602efa9631857679c339cba
Komal Ahluwalia <komalahluwalia06@gmail.com>
2018-12-20 09:20:54 +0100

    Add start server script to run it in pm2 task

--- server/devops/ecosystem.config.js
+++ server/devops/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'server',
     script: './scripts/run_with_credstash.sh',
-    args: 'npm run startServer',
+    args: 'npm run startserver',
     env: {
       SERVER_PORT: 8081,
       NODE_ENV: 'development',
