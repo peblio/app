775f698bd7371569b04fe105a400aa672b4d5105
Komal Ahluwalia <komalahluwalia06@gmail.com>
2018-12-30 09:23:47 +0100

    Use new create user method after refactoring

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 const shortid = require('shortid');
 const { OAuth2Client } = require('google-auth-library');
+const { createUser } = require('./userControllerNew.js');
 
 const User = require('../models/user.js');
 const Token = require('../models/token.js');
@@ -84,68 +85,6 @@ function sendResetMail(email, users, tokens, req) {
 }
 
 // Methods: AUTH
-function createUser(req, res) {
-  const email = req.body.mail;
-  const name = req.body.name;
-  const type = req.body.userType;
-  const password = req.body.password;
-  const requiresGuardianConsent = req.body.requiresGuardianConsent;
-  const guardianEmail = req.body.guardianEmail;
-  const guardianConsentedAt = (requiresGuardianConsent === true) ? new Date() : '';
-  const isVerified = (type === 'student');
-  return User.findOne({ name }, (userFindViaNameError, userByName) => {
-    if (userByName) {
-      return res.status(400).send({
-        msg: UserConst.SIGN_UP_DUPLICATE_USER
-      });
-    }
-
-    let user = null;
-    user = new User({
-      email,
-      name,
-      type,
-      password,
-      loginType: 'password',
-      requiresGuardianConsent,
-      guardianEmail,
-      guardianConsentedAt,
-      isVerified
-    });
-    user.hashPassword(password);
-    return user.save((updateUserError, updatedUser) => {
-      if (updateUserError) {
-        res.status(422).json({
-          msg: UserConst.SIGN_UP_FAILED
-        });
-      }
-
-      if (isVerified) {
-        return res.status(200).send({
-          msg: UserConst.PROCEED_TO_LOG_IN, user
-        });
-      }
-
-      const token = new Token({
-        _userId: updatedUser._id,
-        token: shortid.generate()
-      });
-      token.save((updateTokenError) => {
-        if (updateTokenError) {
-          return res.status(500).send({
-            msg: UserConst.SIGN_UP_FAILED
-          });
-        }
-        return sendSignUpConfirmationMail(updatedUser.email, [name], [token.token], req);
-      });
-
-      return res.status(200).send({
-        msg: UserConst.SIGN_UP_CHECK_MAIL, user
-      });
-    });
-  });
-}
-
 function forgotPassword(req, res) {
   User.find({ email: req.body.email }, (userFindError, users) => {
     const userNames = [];
