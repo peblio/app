af218161febaffbda8ef1fda98ca45074c83778a
Komal Ahluwalia <komalahluwalia06@gmail.com>
2019-04-04 14:39:38 +0200

    First UI unit test - Initial Commit - WIP

--- client/test/app/components/App/Canvas/Canvas-test.js
+++ client/test/app/components/App/Canvas/Canvas-test.js
@@ -1,37 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import { expect } from 'chai';
+const sinon = require('sinon');
+const sandbox = sinon.sandbox.create();
 import Adapter from 'enzyme-adapter-react-16';
-
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
-import Canvas from '../../../../../src/app/components/App/Canvas/Canvas.jsx';
-import Heading from '../../../../../src/app/components/App/Canvas/Heading/Heading.jsx';
-
-
+import Tags from '../../../../../src/app/components/App/Canvas/Tags/Tags.jsx';
 const mockStore = configureMockStore();
-const store = mockStore({});
-
+const store = mockStore({
+  page: {
+    id: 1,
+    tags: ["java", "java8"]
+  }
+});
+let addPageTagMock = sandbox.mock();
+let deletePageTagMock = sandbox.mock();
+const props = {
+  container: "canvas",
+  preview: false,
+  addPageTag: addPageTagMock,
+  deletePageTag: deletePageTagMock
+};
 configure({ adapter: new Adapter() });
 
-
 describe('Canvas Component', () => {
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('should render without throwing an error', () => {
-    const wrapper = shallow(
+    const wrapper = mount(
       <Provider store={store}>
-        <Canvas />
-      </Provider>
+        <Tags {...props} />
+      </Provider>,
+      {
+        context: { store }
+    }
     );
-    expect(wrapper.find(Heading)).to.have.lengthOf(1);
+    expect(wrapper.find(Tags)).to.have.lengthOf(1);
+    expect(wrapper.find('.tags__container')).to.have.lengthOf(1);
+    expect(wrapper.find('.tags__container--canvas')).to.have.lengthOf(1);
   });
 });
-//
-// describe('Canvas component testing', function() {
-//   it('renders Canvas message', function() {
-//     const wrapper = shallow(<Canvas />);
-//
-//     console.log("wrapper", wrapper);
-//     expect(wrapper.contains(welcome)).to.equal(true);
-//   });
-// });
