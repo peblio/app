e84f0a431c7de3b62fa419170ccd55e8b61f7e85
Komal Ahluwalia <komalahluwalia06@gmail.com>
2019-05-20 10:56:30 +0530

    Use wss for secure socket communication

--- client/src/app/utils/webSockets.js
+++ client/src/app/utils/webSockets.js
@@ -1,4 +1,4 @@
 const serverUrl = process.env.SERVER_API_DOMAIN && process.env.SERVER_API_DOMAIN !== ''
-  ? process.env.SERVER_API_DOMAIN
-  : 'localhost:8081';
+  ? `wss://${process.env.SERVER_API_DOMAIN}`
+  : 'ws://localhost:8081';
 export default serverUrl;
