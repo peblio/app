c43d208cc889f52cfdca2037b289ae6e3417ec3d
Komal Ahluwalia <komalahluwalia06@gmail.com>
2019-05-27 18:41:10 +0530

    Upgrade to mongoose-aggregate-paginate-v2 since it gives the ability to get totalDocs in aggregatePaginate response

--- server/src/models/page.js
+++ server/src/models/page.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-aggregate-paginate');
+const mongoosePaginate = require('mongoose-aggregate-paginate-v2');
 
 const Schema = mongoose.Schema;
 
