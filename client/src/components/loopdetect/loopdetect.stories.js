47af91c65f6a4b7179f30b29b454346ba601b030
Komal Ahluwalia <komalahluwalia06@gmail.com>
2020-07-03 14:50:03 +0530

    Remove stale components

--- client/src/components/loopdetect/loopdetect.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import InfiniteLoopDetectableCode from '../../app/components/App/Shared/EditorComponents/CodeOutput/InfiniteLoopDetectableCode.js';
-
-export const infiniteLoopDetectableCode = () => (
-  <InfiniteLoopDetectableCode />
-);
