7a8ba572dc7941ec33a9b901d39d1e14c6ced577
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-07-13 16:52:24 +0530

    Mocked up join class model

--- client/src/app/components/Modal/Modal.jsx
+++ client/src/app/components/Modal/Modal.jsx
@@ -6,6 +6,7 @@ const Modal = ({
   modalWidth,
   modalHeight,
   modalClose,
+  modalClass,
   modalBodyStyle,
   header,
   children,
@@ -13,11 +14,11 @@ const Modal = ({
 }) => (
   <div className="modal">
     <div className="modal__overlay">
-      <div className="modal__box" style={{ width: modalWidth }}>
-        <div className="modal__header">
+      <div className={`modal__box ${modalClass || ''}`} style={{ width: modalWidth }}>
+        <div className={`modal__header ${modalClass ? `${modalClass}__header` : ''}`}>
           {header}
         </div>
-        <div className="modal__body" style={modalBodyStyle}>
+        <div className={`modal__body ${modalClass ? `${modalClass}__body` : ''}`} style={modalBodyStyle}>
           {children}
         </div>
       </div>
@@ -29,9 +30,14 @@ Modal.propTypes = {
   modalWidth: PropTypes.string.isRequired,
   modalHeight: PropTypes.string.isRequired,
   modalClose: PropTypes.func.isRequired,
+  modalClass: PropTypes.string,
   modalBodyStyle: PropTypes.shape({}).isRequired,
   header: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+};
+
+Modal.defaultProps = {
+  modalClass: ''
 };
 
 export default Modal;
