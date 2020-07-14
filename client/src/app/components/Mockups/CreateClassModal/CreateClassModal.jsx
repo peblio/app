32685843b8c67d5c1cea58cf91bc47dc3f2eb689
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-07-14 16:01:18 +0530

    Modified modal component for better reusability and removed redundant props | added tests for generic modal

--- client/src/app/components/Mockups/CreateClassModal/CreateClassModal.jsx
+++ client/src/app/components/Mockups/CreateClassModal/CreateClassModal.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import Modal from '../../Modal/Modal';
 import InputField from '../../InputField/InputField';
 import Dropdown from '../../Dropdown/Dropdown';
 import TextareaField from '../../TextareaField/TextareaField';
 import Button from '../../Button/Button';
+
+import './createClassModal.scss';
 
 export const CreateClassroomModal = () => {
   const [className, setClassName] = useState('');
@@ -13,10 +15,6 @@ export const CreateClassroomModal = () => {
   const [grade, setGrade] = useState('');
   const [description, setDescription] = useState('');
   const [isCreateEnabled, setIsCreateEnabled] = useState(false);
-
-  useEffect(() => {
-    console.log('create', isCreateEnabled);
-  });
 
   const onClassNameChange = (e) => {
     const value = e.target.value.trim();
@@ -29,85 +27,87 @@ export const CreateClassroomModal = () => {
   };
 
   return (
-    <Modal header="Create Class" modalWidth="893px" modalBodyStyle={{ position: 'relative' }}>
-      <div style={{
-        position: 'absolute',
-        top: '13px',
-        right: '26px',
-        color: '#667378',
-        fontSize: '14px'
-      }}
-      >
+    <Modal
+      header="Create Class"
+      modalBodyStyle={{ position: 'relative' }}
+      modalClass="create-class-modal"
+    >
+      <div className="create-class-modal__required">
         *required
       </div>
-      <InputField
-        state={className}
-        onChange={onClassNameChange}
-        label="*Class Name"
-        placeholder="enter class name"
-        containerWidth="813px"
-      />
-      <div className="" style={{ display: 'flex', marginTop: '40px' }}>
-        <Dropdown
-          placeholder="Grade"
-          style={{ width: '100px', marginTop: '28px', marginRight: '30px' }}
-          state={grade}
-          setState={setGrade}
-          options={[
-            {
-              name: '10th',
-              value: 10
-            },
-            {
-              name: '9th',
-              value: 9
-            },
-            {
-              name: '8th',
-              value: 8
-            }, {
-              name: '7th',
-              value: 7
-            },
-            {
-              name: '6th Standard',
-              value: 6
-            }
-          ]}
-        />
-        <InputField
-          state={room}
-          onChange={(e) => { setRoom(e.target.value.trim()); }}
-          label="Room"
-          placeholder="enter subject"
-          containerWidth="199px"
-          style={{ marginRight: '30px' }}
-        />
-        <InputField
-          state={subject}
-          onChange={(e) => { setSubject(e.target.value.trim()); }}
-          label="Subject"
-          placeholder="enter subject"
-          containerWidth="199px"
-        />
-      </div>
-      <TextareaField
-        state={description}
-        onChange={(e) => { setDescription(e.target.value.trim()); }}
-        label="Description"
-        placeholder="type description..."
-        containerWidth="813px"
-        textareaHeight="96px"
-        style={{ marginTop: '40px' }}
-      />
-      <div style={{ display: 'flex', marginTop: '40px' }}>
-        <Button className="secondary" style={{ marginLeft: 'auto', marginRight: '16px' }}>
-          Cancel
-        </Button>
-        <Button className="primary" disabled={!isCreateEnabled}>
-          Create class
-        </Button>
-      </div>
+      <form action="">
+        <div className="create-class-modal__row">
+          <InputField
+            state={className}
+            onChange={onClassNameChange}
+            label="*Class Name"
+            placeholder="enter class name"
+            containerWidth="100%"
+          />
+        </div>
+        <div className="create-class-modal__row">
+          <Dropdown
+            placeholder="Grade"
+            style={{ width: '100px', marginTop: '28px', marginRight: '30px' }}
+            state={grade}
+            setState={setGrade}
+            options={[
+              {
+                name: '10th',
+                value: 10
+              },
+              {
+                name: '9th',
+                value: 9
+              },
+              {
+                name: '8th',
+                value: 8
+              }, {
+                name: '7th',
+                value: 7
+              },
+              {
+                name: '6th Standard',
+                value: 6
+              }
+            ]}
+          />
+          <InputField
+            state={room}
+            onChange={(e) => { setRoom(e.target.value.trim()); }}
+            label="Room"
+            placeholder="enter subject"
+            containerWidth="199px"
+            style={{ marginRight: '30px' }}
+          />
+          <InputField
+            state={subject}
+            onChange={(e) => { setSubject(e.target.value.trim()); }}
+            label="Subject"
+            placeholder="enter subject"
+            containerWidth="199px"
+          />
+        </div>
+        <div className="create-class-modal__row">
+          <TextareaField
+            state={description}
+            onChange={(e) => { setDescription(e.target.value.trim()); }}
+            label="Description"
+            placeholder="type description..."
+            containerWidth="100%"
+            textareaHeight="96px"
+          />
+        </div>
+        <div className="create-class-modal__row">
+          <Button className="secondary" style={{ marginLeft: 'auto', marginRight: '16px' }}>
+            Cancel
+          </Button>
+          <Button className="primary" disabled={!isCreateEnabled}>
+            Create class
+          </Button>
+        </div>
+      </form>
     </Modal>
   );
 };
---
+++ client/src/app/constants/modalConstants.js
@@ -0,0 +1,2 @@
+export const DEFAULT_MODAL_WIDTH = '557px';
+export const CREATE_CLASS_MODAL_WIDTH = '893px';
