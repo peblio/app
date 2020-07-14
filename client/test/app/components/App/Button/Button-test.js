ce86182adb8418b83fa13f0c31f64d210a10d3ff
Sudeep Gumaste <sudeepgumaste19@gmail.com>
2020-07-14 12:10:55 +0530

    Refined button tests

--- client/test/app/components/App/Button/Button-test.js
+++ client/test/app/components/App/Button/Button-test.js
@@ -4,19 +4,24 @@ import { expect } from 'chai';
 
 import Button from '../../../../../src/app/components/Button/Button';
 
-const setUp = (props = {}) => {
-  const component = shallow(<Button {...props} />);
-  return component;
-};
-
 describe('Button component', () => {
   it('Should render without errors', () => {
-    const wrapper = setUp();
+    const wrapper = shallow(
+      <Button>
+        Hello
+      </Button>
+    );
     expect(wrapper.find('.button')).to.have.lengthOf(1);
+    expect(wrapper.text()).to.equal('Hello');
   });
 
   it('Should also have a className supplied as a prop', () => {
-    const wrapper = setUp({ className: 'primary' });
+    const wrapper = shallow(
+      <Button className="primary">
+        Hello
+      </Button>
+    );
     expect(wrapper.find('.button.primary')).to.have.lengthOf(1);
+    expect(wrapper.text()).to.equal('Hello');
   });
 });
