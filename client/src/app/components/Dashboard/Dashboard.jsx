8e65cac2b238351926b4f90145e991bbe7784fae
Komal Ahluwalia <komalahluwalia06@gmail.com>
2020-07-20 13:53:00 +0530

    Issue 857 (#862)
    * Fix display of left nav menu on dashbaord to avoid gray patch when page is scrolled
    
    * Keey top navigation sticky when page is scrolled
    
    * Revert "Keey top navigation sticky when page is scrolled"
    
    This reverts commit af0abb32d11fcf7c798fc3e8c3ebd0f0d666dbff.
    
    * Set background color of dashbaord to be g8 ti avoid seeing a grey patch

--- client/src/app/components/Dashboard/Dashboard.jsx
+++ client/src/app/components/Dashboard/Dashboard.jsx
@@ -94,7 +94,7 @@ class Dashboard extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="dashboard">
         <TopNav
           container="dashboard"
         />
