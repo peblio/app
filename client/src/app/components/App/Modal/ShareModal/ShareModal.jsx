703ecb785dcff88bbe4fba07e1495dc220b4277b
Komal Ahluwalia <komalahluwalia06@gmail.com>
2020-11-06 09:03:22 +0530

    Show View Only as selected for student

--- client/src/app/components/App/Modal/ShareModal/ShareModal.jsx
+++ client/src/app/components/App/Modal/ShareModal/ShareModal.jsx
@@ -19,6 +19,28 @@ class Share extends React.Component {
     this.renderClassroomWidget();
   }
 
+  getOptions = () => {
+    if (this.props.type === 'student') {
+      return [{
+        value: 'link',
+        displayKey: 'View only',
+        selected: true,
+      }, {
+        value: 'autoRemixLink',
+        displayKey: 'Share as assignment',
+      }];
+    }
+    return [{
+      value: 'autoRemixLink',
+      displayKey: 'Share as assignment',
+      selected: true,
+    },
+    {
+      value: 'link',
+      displayKey: 'View only',
+    }];
+  }
+
   linkTypeChanged = (e) => {
     const linkType = e.target.value;
     this.setState({ linkType });
@@ -39,16 +61,8 @@ class Share extends React.Component {
         url: this.props.shareURL });
   }
 
+
   render() {
-    const options = [{
-      value: 'autoRemixLink',
-      displayKey: 'Share as assignment',
-      selected: true,
-    },
-    {
-      value: 'link',
-      displayKey: 'View only',
-    }];
     return (
       <section className="share__container">
         <div className="share__option">
@@ -57,7 +71,7 @@ class Share extends React.Component {
           </h2>
           <div className="share__option__container">
             <Dropdown
-              options={options}
+              options={this.getOptions()}
               onChange={this.linkTypeChanged}
               formLabel="Share Options"
               formControlClassName="share-modal__select"
@@ -108,10 +122,12 @@ class Share extends React.Component {
 Share.propTypes = {
   pageTitle: PropTypes.string.isRequired,
   shareURL: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
-  shareURL: state.mainToolbar.shareURL
+  shareURL: state.mainToolbar.shareURL,
+  type: state.user.type
 });
 
 export default connect(mapStateToProps, null)(Share);
