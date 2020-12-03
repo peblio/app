87203c6a9de7547da2e34d6173b42930e0f9e176
Komal Ahluwalia <komalahluwalia06@gmail.com>
2020-12-04 02:38:20 +0530

    Remove unused method

--- server/src/service/classroomService.js
+++ server/src/service/classroomService.js
@@ -9,36 +9,6 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 const url = require('url');
 const User = require('../models/user.js');
 
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }), (request, response) => {
-  let event;
-
-  try {
-    event = JSON.parse(request.body);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntent = event.data.object;
-      // Then define and call a method to handle the successful payment intent.
-      // handlePaymentIntentSucceeded(paymentIntent);
-      break;
-    case 'payment_method.attached':
-      const paymentMethod = event.data.object;
-      // Then define and call a method to handle the successful attachment of a PaymentMethod.
-      // handlePaymentMethodAttached(paymentMethod);
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a response to acknowledge receipt of the event
-  response.json({ received: true });
-});
-
 export async function createClassroomDetail(req, res) {
   try {
     const classroomDetail = buildClassroomDetailFromRequest(req, req.user._id.toString());
