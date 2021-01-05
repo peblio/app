d79bf82100d4ac30cd7b8ddc8fbccbac01b5b7f1
Komal Ahluwalia <komalahluwalia06@gmail.com>
2021-01-05 10:46:42 +0530

    Show 0 bytes when no memory consumed

--- client/src/app/components/ProgressBar/ProgressBar.jsx
+++ client/src/app/components/ProgressBar/ProgressBar.jsx
@@ -16,7 +16,7 @@ const ProgressBar = ({
 
   function bytesToSize(bytes) {
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
+    if (bytes === 0) return '0 Bytes';
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
     if (i === 0) return `${bytes} ${sizes[i]})`;
     return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
