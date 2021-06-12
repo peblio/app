c3ec1124f36fd084d158a1f6feef490bc105dfa0
Komal Ahluwalia <komalahluwalia06@gmail.com>
2021-06-12 20:28:15 +0530

    Create basic python repl wrapper - WIP

---
+++ client/src/app/components/App/Shared/EditorComponents/PythonRepl/PythonRepl.jsx
@@ -0,0 +1,70 @@
+import { ReactReplView } from 'awesome-react-repl';
+import React from 'react';
+import Sk from 'skulpt';
+
+
+class PythonRepl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lines: []
+    };
+  }
+
+  componentDidMount() {
+    Sk.configure({ output: this.outf, read: this.builtinRead, retainglobals: true });
+    console.log('Configured');
+  }
+
+  outf = (text) => {
+    const lines = this.state.lines;
+    lines.push({ type: 'output', value: text });
+    this.setState({ lines });
+  }
+
+  onSubmit = (input) => {
+    const lines = this.state.lines;
+    lines.push({ type: 'input', value: input });
+    this.setState({ lines });
+
+    const myPromise = Sk.misceval.asyncToPromise(() => Sk.importMainWithBody('repl', false, input, true));
+    myPromise.then((mod) => {
+      console.log('success');
+    },
+    (err) => {
+      const lines = this.state.lines;
+      lines.push({ type: 'error', value: err.toString() });
+      this.setState({ lines });
+    });
+  }
+
+
+  builtinRead = (x) => {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles.files[x] === undefined) {
+      throw new Error(`File not found: '${x}'`);
+    }
+    return Sk.builtinFiles.files[x];
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactReplView
+          title="My Awesome Repl!"
+          tabs={['Python']}
+          selectedTab="Python"
+          onSubmit={this.onSubmit}
+          onClear={console.log('Cleared')}
+          height={200}
+          lines={this.state.lines}
+        />
+      </div>
+    );
+  }
+}
+
+PythonRepl.propTypes = {
+};
+
+
+export default PythonRepl;
