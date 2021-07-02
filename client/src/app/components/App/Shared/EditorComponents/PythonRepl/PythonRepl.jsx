f91a2740f24f98289832a1d1286b3e17c0216ac7
Komal Ahluwalia <komalahluwalia06@gmail.com>
2021-07-02 21:58:20 +0530

    Revert "Make repl run every command from the beginning when a new command is fired. This is to support multiple repls - Bugg commit - Please fix or revert asap"
    This reverts commit 777b6a169031dab1c6a8898c13cb1205cd7e56c6.

--- client/src/app/components/App/Shared/EditorComponents/PythonRepl/PythonRepl.jsx
+++ client/src/app/components/App/Shared/EditorComponents/PythonRepl/PythonRepl.jsx
@@ -7,36 +7,30 @@ class PythonRepl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      lines: [],
-      inputLines: [],
+      lines: []
     };
   }
 
   outf = (text) => {
     const lines = this.state.lines;
     lines.push({ type: 'output', value: text });
-    this.setState(prevState => ({ lines, inputLines: prevState.inputLines }));
+    this.setState({ lines });
   }
 
   onSubmit = (input) => {
     Sk.configure({ output: this.outf, read: this.builtinRead, retainglobals: true });
-    const inputLines = this.state.inputLines;
-    inputLines.push({ type: 'input', value: input });
-    this.setState(() => ({ lines: [], inputLines }));
-    inputLines.forEach((inputLine) => {
+    const lines = this.state.lines;
+    lines.push({ type: 'input', value: input });
+    this.setState({ lines });
+
+    const myPromise = Sk.misceval.asyncToPromise(() => Sk.importMainWithBody('repl', false, input, true));
+    myPromise.then((mod) => {
+      console.log('success');
+    },
+    (err) => {
       const lines = this.state.lines;
-      lines.push({ type: 'input', value: input });
-      this.setState(prevState => ({ lines, inputLines: prevState.inputLines }));
-      console.log('pushing input', lines);
-      const myPromise = Sk.misceval.asyncToPromise(() => Sk.importMainWithBody('repl', false, inputLine.value, true));
-      myPromise.then((mod) => {
-        console.log('success');
-      },
-      (err) => {
-        const existingLines = this.state.lines;
-        existingLines.push({ type: 'error', value: err.toString() });
-        this.setState(prevState => ({ lines: existingLines, inputLines: prevState.inputLines }));
-      });
+      lines.push({ type: 'error', value: err.toString() });
+      this.setState({ lines });
     });
   }
 
