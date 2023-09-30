f63b4a940ff95fe6382c611a4faae15c7629b7a1
Komal Ahluwalia <komalahluwalia06@gmail.com>
2023-09-30 17:46:35 +0000

    Upgrade mongodb driver and node version to support mongodb atlas 6 ve… (#919)
    * Upgrade mongodb driver and node version to support mongodb atlas 6 version
    
    * Remove accidental console log

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,11 @@ let app = express();
 const expressWs = require('express-ws')(app);
 app = expressWs.app;
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
+const store = new MongoStore({
+  mongoUrl: process.env.MONGO_DB_PEBLIO,
+  autoReconnect: true
+})
 const currentUserRoutes = require('./controllers/currentUserController');
 const webSocketRoutes = require('./routes/webSocketRoutes.js')(expressWs);
 const pageRoutes = require('./routes/pageRoutes.js');
@@ -42,10 +46,7 @@ app.use(session({
     httpOnly: false,
     secure: false,
   },
-  store: new MongoStore({
-    url: process.env.MONGO_DB_PEBLIO,
-    autoReconnect: true
-  })
+  store: store
 }));
 
 // add body parser
